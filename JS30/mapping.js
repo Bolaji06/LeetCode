30953dd0990468ea152cf4a069b69b27b27a8e15
Bolaji06 <joshspencer0603@gmail.com>
2023-12-21 07:01:35 +0100

    changes

--- JS30/mapping.js
+++ JS30/mapping.js
@@ -1,14 +1,23 @@
 
+// Given an integer array arr and a mapping function fn,
+// return a new array with a transformation applied to each element.
 
-function map (arr, fn){
+// The returned array should be created such that returnedArray[i] = fn(arr[i], i).
+
+// Please solve it without the built-in Array.map method.
+
+function map(arr, fn){
+    // Create a new array
     let transformedArr = [];
-    for(let i = 0; i < arr.length; i++) {
+    // Step through the array for each element in transformedArr create item with 'fn' function
+    for (let i = 0; i < arr.length; i++){
         transformedArr[i] = fn(arr[i], i);
     }
     return transformedArr;
 }
-function fn (num, i) {
-   return num * num + i  
+
+function doubleWithIndex(arr, i){
+    return arr * arr + i
 }
-const result = map([1, 2, 3], fn);
+const result = map([1, 2, 3], doubleWithIndex);
 console.log(result);
